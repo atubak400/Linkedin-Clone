0518ffa37eb8831fe5d5891dfb1f9093302158ea
Kingsley Atuba <atubak40@gmail.com>
2021-08-14 11:37:44 +0100

    building sidebar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './App.css'
 import Header from '../src/Components/Header/Header'
 import '../src/Components/Header/Header.css'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Header />
+      <div className='app__body'>
+        <Sidebar />
+      </div>
     </div>
   )
 }
