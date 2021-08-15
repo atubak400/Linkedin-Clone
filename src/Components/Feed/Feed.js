27ff37ed3a76115dfcbd472ba9bbbf245c70dba4
Kingsley Atuba <atubak40@gmail.com>
2021-08-15 21:32:19 +0100

    Feed input Section Done

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,7 +1,12 @@
 import './feed.css'
-
+import ImageIcon from '@material-ui/icons/Image'
 import React from 'react'
 import CreateIcon from '@material-ui/icons/Create'
+import InputOption from '../InputOption/InputOption'
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
+
 function Feed() {
   return (
     <div className='feed'>
@@ -12,6 +17,20 @@ function Feed() {
             <input type='text' />
             <button type='submit'>Send</button>
           </form>
+        </div>
+        <div className='feed__inputOptions'>
+          <InputOption Icon={ImageIcon} title='Photo' color='#70b5f9' />
+          <InputOption
+            Icon={SubscriptionsIcon}
+            title='Video'
+            color='#7E7A33E'
+          />
+          <InputOption Icon={EventNoteIcon} title='Event' color='#C0CBCD' />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title='Write article'
+            color='#7FC15E'
+          />
         </div>
       </div>
     </div>
